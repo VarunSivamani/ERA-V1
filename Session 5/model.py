6e5cb864db5025b7c23dd017b58ec3e09f9cb17d
VarunSivamani <varunsivamani@gmail.com>
2023-06-02 12:45:20 +0530

    model.py file

---
+++ Session 5/model.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# CODE BLOCK: 7
+class Net(nn.Module): 
+    # This defines the structure of the NN.
+    def __init__(self) -> None:
+        '''
+        Initializes a Net instance.
+
+        Returns:
+        - None
+
+        '''
+
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.fc1 = nn.Linear(256 * 4 * 4, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        Performs a forward pass of the model on input tensor x.
+            
+        Args:
+        - x: input tensor 
+        
+        Returns:
+        - output tensor
+        
+        '''
+                                             #  Ch           RF        J_out
+        x = F.relu(self.conv1(x))            # 28>26    |   1>3     |  1>1
+        x = F.max_pool2d(self.conv2(x), 2)   # 26>24>12 |   3>5>6   |  1>1>2
+        x = F.relu(self.conv3(x))            # 12>10    |   6>10    |  2>2
+        x = F.max_pool2d(self.conv4(x), 2)   # 10>8>4   |  10>14>16 |  2>2>4
+        x = x.view(x.size(0), -1)  # 256 * 4 * 4 = 4096
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        
+        return F.log_softmax(x, dim=1)
